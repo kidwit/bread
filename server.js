a5982f27b5c6b2ebbd33ae516a844b8f5b604419
Travis Jones <toj817@gmail.com>
2024-03-03 11:31:26 -0800

    created a server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+require('dotenv').config()
+const breadRoutes = require('./controllers/bread')
+
+const app = express()
+
+app.use('/bread', breadRoutes)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
